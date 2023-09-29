968f59893bd46aaa9f38c7d6c4d69b3d1a23e23b
James Lau <hkjl87@hotmail.co.uk>
2023-09-29 19:29:15 +0900

    v-for

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { reactive } from 'vue';
 import TaskCard from './TaskCard.vue';
+
+const tasks = reactive([
+{
+    title: "Create a card component",
+    description:
+      "Create a new TaskCard.vue file in the components folder, then import it in TasksList",
+    done: true,
+  },
+  {
+    title: "Make the card component dynamic",
+    description:
+      "Learn about using the data option and passing data to child components using props",
+    done: true,
+  },
+  {
+    title: "Bind the attributes to the data",
+    description:
+      "Use the v-bind directive to bind the title and description to our data",
+    done: false,
+  }
+])
 </script>
 
 <template>
-  <TaskCard title = "My first task" description="The 1st one"/>
-  <TaskCard title = "My second task" description="The 1st one"/>
-  <TaskCard title = "My third task" description="The 1st one"/>
-
-
+  <TaskCard v-for="(task, index) in tasks" :title="task.title" :description="task.description" :done="task.done"/>
 </template>
