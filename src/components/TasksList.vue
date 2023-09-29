73f51e0b9906c959ab0a7b40a74e1dfbd4d9aa2e
James Lau <hkjl87@hotmail.co.uk>
2023-09-29 20:26:12 +0900

    ref

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,31 +1,26 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import TaskCard from './TaskCard.vue';
 
-const tasks = reactive([
-// {
-//     title: "Create a card component",
-//     description:
-//       "Create a new TaskCard.vue file in the components folder, then import it in TasksList",
-//     done: true,
-//   },
-//   {
-//     title: "Make the card component dynamic",
-//     description:
-//       "Learn about using the data option and passing data to child components using props",
-//     done: true,
-//   },
-//   {
-//     title: "Bind the attributes to the data",
-//     description:
-//       "Use the v-bind directive to bind the title and description to our data",
-//     done: false,
-//   }
-])
+const tasks = reactive([])
+function addTask(title, description) {
+  tasks.unshift({title, description});
+
+}
+
+const newTitle = ref('')
+const newDescription = ref('')
 </script>
 
 <template>
-  <button class="btn round-icon" v-on:click="console.log('clicked')">＋</button>
+  <button class="btn round-icon" @click="addTask(newTitle, newDescription)">＋</button>
+  <div class="task-card new-task">
+    <div>
+      <input type="text" placeholder="What would you like to do?" />
+      <textarea placeholder="Add some details about your task..."></textarea>
+    </div>
+  </div>
+
   <TaskCard v-if="tasks.length > 0" v-for="(task, index) in tasks" :title="task.title" :description="task.description" :done="task.done"/>
     <p v-else>You don't have any task yet...</p>
 </template>
